cbe32939b3e4e38247233ebadcd2c23a2c5ecd7f
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-21 17:13:18 +0300

    add input style

---
+++ src/components/inputlist/input.style.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const LabelInput = styled.label`
+    font-size: 1.5rem;
+    color: black;
+    font-weight: bolder;
+`;
+
+export const InputText = styled.input`
+    font-size: 2rem;
+    width: 40rem;
+    padding: 0.5rem 1rem;
+`;
+
+export const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
