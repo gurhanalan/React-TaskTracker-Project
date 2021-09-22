a841c8e1b8232f332f2e67fb64cf17a6c319130b
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-22 17:14:14 +0300

    add main wrapper

---
+++ src/components/main-wrapper/WrapperMain.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const WrapperMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem;
+    background-image: linear-gradient(135deg, #90f7ec 10%, #32ccbc 100%);
+    max-width: 60rem;
+    margin: 2rem auto;
+    border-radius: 1rem;
+    box-shadow: 0 3rem 4rem hsla(0, 0%, 0%, 0.3);
+`;
