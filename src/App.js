c7b759de6ad6047986ae1c155edab4cc5b9679a0
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-22 17:14:39 +0300

    change in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
+import React, { useState } from "react";
+
 import Header from "./components/header/Header";
+import InputList from "./components/inputlist/InputList";
+import { WrapperMain } from "./components/main-wrapper/WrapperMain";
+import TaskList from "./components/tasklist/TaskList";
+
+const taskArray = [
+    {
+        id: "1",
+        task: "Study React",
+        date: "Feb 5th at 2:30pm",
+        isDone: false,
+    },
+    {
+        id: "2",
+        task: "Study Javascript",
+        date: "Feb 5th at 6:30pm",
+        isDone: false,
+    },
+];
 
 function App() {
+    const [tasks, setTasks] = useState(taskArray);
+
+    const addTask = (newTask) => setTasks([...tasks, newTask]);
     return (
         <div className="App">
-            <Header />
+            <WrapperMain>
+                <Header />
+                <InputList addTask={addTask}/>
+                <TaskList taskArray={tasks} />
+            </WrapperMain>
         </div>
     );
 }
