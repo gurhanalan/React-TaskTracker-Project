3b0dde42feaf1a22f6797cc05edc4ee90666281e
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-23 01:11:40 +0300

    Change Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { ButtonHeader, Title, Wrapper } from "./Header.style";
+import { colors } from "./Header.style";
 
-function Header() {
+function Header({ toggleInputDisplay, showInputField }) {
     return (
         <Wrapper>
             <Title>Task Tracker</Title>
-            <ButtonHeader>Hide Task Bar</ButtonHeader>
+            <ButtonHeader
+                onClick={toggleInputDisplay}
+                style={{
+                    backgroundColor: `${
+                        showInputField ? colors.color2 : colors.color3
+                    }`,
+                }}
+            >
+                {showInputField ? "Hide Task Bar" : "Show Task Bar"}
+            </ButtonHeader>
         </Wrapper>
     );
 }
