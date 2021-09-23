9988b048b33089617ee4666d2a073ea37d4a90c4
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-24 01:14:21 +0300

    change wrapper main

--- src/components/main-wrapper/WrapperMain.jsx
+++ src/components/main-wrapper/WrapperMain.jsx
@@ -6,7 +6,7 @@ export const WrapperMain = styled.div`
     gap: 1rem;
     padding: 2rem;
     background-image: linear-gradient(135deg, #90f7ec 10%, #32ccbc 100%);
-    max-width: 60rem;
+    max-width: 50rem;
     margin: 2rem auto;
     border-radius: 1rem;
     box-shadow: 0 3rem 4rem hsla(0, 0%, 0%, 0.3);
