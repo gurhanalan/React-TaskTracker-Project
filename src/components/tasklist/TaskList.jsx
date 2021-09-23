26cc84222f7ed07e8ca0736d5cc27803b3a7624d
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-24 01:15:00 +0300

    modify tasklist

--- src/components/tasklist/TaskList.jsx
+++ src/components/tasklist/TaskList.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Wrapper } from "../header/Header.style";
 import Task from "./Task";
 
-function TaskList({ taskArray }) {
+function TaskList({ taskArray, toggleIsDone }) {
     return (
         <Wrapper>
-            {taskArray.map((item) => (
-                <Task key={item.id} item={item} />
-            ))}
+            {taskArray.filter((item) => !item.isDone).length === 0
+                ? "No task to show"
+                : taskArray.map(
+                      (item) =>
+                          !item.isDone && (
+                              <Task
+                                  key={item.id}
+                                  item={item}
+                                  toggleIsDone={toggleIsDone}
+                              />
+                          )
+                  )}
         </Wrapper>
     );
 }
